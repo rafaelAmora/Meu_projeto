505d375a1c5330e97b25bb9b07c9a94752bc2227
rafaelAmora <rmoreira2209@gmail.com>
2023-06-08 21:07:56 -0300

    exercicio1

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // alert('alerta de texto');
 
-const body = document.getElementById("body");
-const btt = document.querySelector(".button-color");
+const body = document.getElementById("body");/* Busca a id*/ 
+const btt = document.querySelector(".button-seta"); /*busca a clss*/ 
 const caixa = document.querySelector(".caixa_principal");
+const seta = document.querySelector(".button-seta");
+const footer = document.querySelector(".footer")
+const insta = document.querySelector(".social")
+const lista = document.querySelector(".face")
+
 
 btt.addEventListener('click', () => {
-  body.classList.toggle('light');
-  caixa.classList.toggle('light');
+  body.classList.toggle('light'); /*Adiciona a classe*/ 
+  caixa.classList.toggle('light'); /*toogle significa trocar */
+  seta.classList.toggle('light')
+ footer.classList.toggle('light')
+ insta.classList.toggle('light')
+ lista.classList.toggle('light')
 });
 
-console.log(btt);
+console.log(btt); /*Retorno do computador*/
